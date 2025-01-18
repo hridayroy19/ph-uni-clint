2685d0bfabb59e03e90b4743d05694bc28edd797
hridayroy19 <hrhridoyroy503@gmail.com>
2025-01-18 20:44:33 +0600

    redux connact projcet

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import route from './routes/routes.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/features/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <RouterProvider router={route}/>
+    <Provider store={store}>   <RouterProvider router={route}/> </Provider>
   </StrictMode>,
 )
