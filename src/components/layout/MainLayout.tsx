126ead7a644f3beaed33197d622570d93ce45542
hridayroy19 <hrhridoyroy503@gmail.com>
2025-01-19 10:58:22 +0600

    update main layout use role system

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,25 +1,12 @@
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-const { Header, Content, Footer, Sider } = Layout;
+import Sidebar from './Sidebar';
+const { Header, Content} = Layout;
 
 const MainLayout = () => {
     return (
         <Layout style={{height:"100vh"}}>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div style={{height:'4rem', display:'flex', justifyContent:'center', alignItems:'center', color:'white' }}>
-            <h2> PH UNI</h2>
-          </div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={adminSidebarItems} />
-        </Sider>
+        <Sidebar/>
         <Layout>
           <Header style={{ padding: 0,}} />
           <Content style={{ margin: '24px 16px 0' }}>
@@ -32,9 +19,6 @@ const MainLayout = () => {
              <Outlet/>
             </div>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-          </Footer>
         </Layout>
       </Layout>
     );
